c2c426111fa45d35e6734a14c72e2ee09b587337
DrJupiter <klausjupiter@hotmail.com>
2023-02-02 09:57:54 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+## Jax
+
+# Stop jax from taking up 90% of GPU vram
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']='false'
+
+import jax
+
+
+
+## Weights and biases
+import wandb
+
+#wandb.init(project="test-project", entity="ai-dtu")
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+@hydra.main(config_path="configs/", config_name="defaults", version_base='1.3')
+def run_experiment(cfg):
+
+    print(cfg) 
+    wandb.config = OmegaConf.to_container(
+            cfg, resolve=True, throw_on_missing=True
+            )
+
+    run = wandb.init(entity=cfg.wandb.setup.entity, project=cfg.wandb.setup.project)
+    wandb.log({"loss": 0})
+
+if __name__ == "__main__":
+    run_experiment()
