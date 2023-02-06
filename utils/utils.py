c5b6d95365222e51c25d2c16a75a2f7080920d60
DrJupiter <klausjupiter@hotmail.com>
2023-02-06 11:12:40 +0100

    visualization and utils for loading the cfg manually

---
+++ utils/utils.py
@@ -0,0 +1,20 @@
+
+from hydra import compose, initialize
+import hydra
+from omegaconf import OmegaConf
+
+def get_hydra_config(config_path='../configs', job_name='test', version_base='1.3', config='defaults.yaml', overrides=[], reload=True):
+    """
+    !!! This function is meant for TESTING PURPOSES, not to be used
+    in production. !!!
+
+    Load the hydra config manually.
+    
+    (The parameters are the same as loading a hydraconfig normally)
+    """
+
+    if reload:
+       hydra.core.global_hydra.GlobalHydra.instance().clear()
+    initialize(config_path=config_path, job_name=job_name, version_base=version_base) 
+    cfg = compose(config, overrides=overrides)
+    return cfg
