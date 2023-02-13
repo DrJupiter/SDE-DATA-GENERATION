ffb5bfe98b7cd2a191c8deac369d5a5800d8b5f1
DrJupiter <klausjupiter@hotmail.com>
2023-02-13 12:24:10 +0100

    fixed name of batch_size

--- data/dataload.py
+++ data/dataload.py
@@ -61,19 +61,19 @@ def dataload(cfg):
     if name == 'mnist':
 
         mnist_dataset_train = MNIST(cfg.dataset.path, download=True, transform=FlattenAndCast())
-        training_generator = NumpyLoader(mnist_dataset_train, batch_size=cfg.train_and_test.train.batchsize, shuffle=cfg.train_and_test.train.shuffle, num_workers=mp.cpu_count())
+        training_generator = NumpyLoader(mnist_dataset_train, batch_size=cfg.train_and_test.train.batch_size, shuffle=cfg.train_and_test.train.shuffle, num_workers=mp.cpu_count())
 
         mnist_dataset_test = MNIST(cfg.dataset.path, train=False, download=True, transform=FlattenAndCast())
-        test_generator = NumpyLoader(mnist_dataset_test, batch_size=cfg.train_and_test.test.batchsize, shuffle=cfg.train_and_test.test.shuffle, num_workers=mp.cpu_count())
+        test_generator = NumpyLoader(mnist_dataset_test, batch_size=cfg.train_and_test.test.batch_size, shuffle=cfg.train_and_test.test.shuffle, num_workers=mp.cpu_count())
 
         return training_generator, test_generator 
 
     elif name == 'cifar10':
         cifar10_dataset_train = CIFAR10(cfg.dataset.path, download=True, transform=FlattenAndCast())
-        training_generator = NumpyLoader(cifar10_dataset_train, batch_size=cfg.train_and_test.train.batchsize, shuffle=cfg.train_and_test.train.shuffle, num_workers=mp.cpu_count())
+        training_generator = NumpyLoader(cifar10_dataset_train, batch_size=cfg.train_and_test.train.batch_size, shuffle=cfg.train_and_test.train.shuffle, num_workers=mp.cpu_count())
 
         cifar10_dataset_test = CIFAR10(cfg.dataset.path, train=False, download=True, transform=FlattenAndCast())
-        training_generator = NumpyLoader(cifar10_dataset_test, batch_size=cfg.train_and_test.test.batchsize, shuffle=cfg.train_and_test.test.shuffle, num_workers=mp.cpu_count())
+        training_generator = NumpyLoader(cifar10_dataset_test, batch_size=cfg.train_and_test.test.batch_size, shuffle=cfg.train_and_test.test.shuffle, num_workers=mp.cpu_count())
 
         return training_generator, test_generator
     
