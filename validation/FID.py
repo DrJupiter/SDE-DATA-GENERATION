b015663e624769ebabf71aedaffd3623471c724a
Skroko <73649109+Skroko@users.noreply.github.com>
2023-02-13 15:54:45 +0100

    update to FID. Added logging to main and added config for file save location where FID get also get generated imgs

--- validation/FID.py
+++ validation/FID.py
@@ -12,8 +12,8 @@ def FID_score(x1,x2):
     else: 
         path_to_imgs2 = None
 
-    out1 = inceptionv3(x = x1,path_to_imgs = path_to_imgs1) # "./validation/imgs/real/*.jpg"
-    out2 = inceptionv3(x = x2,path_to_imgs = path_to_imgs2) # "./validation/imgs/gen/*.jpg"
+    out1 = inceptionv3(x = x1, path_to_imgs = path_to_imgs1) # "./validation/imgs/real/*.jpg"
+    out2 = inceptionv3(x = x2, path_to_imgs = path_to_imgs2) # "./validation/imgs/gen/*.jpg"
 
     mu1 = out1.mean(dim=0)
     sigma1 = out1.cov(correction=0)
@@ -26,8 +26,9 @@ def FID_score(x1,x2):
     def gaussian_FID(mu1,mu2,sigma1,sigma2):
         m1mm2 = (mu1-mu2).unsqueeze(0)
         mu_dist = torch.mm(m1mm2,torch.transpose(m1mm2,0,1))
+        print(mu_dist)
 
-        sigma = sigma1+sigma2-2*(((sigma1**(0.5))*sigma2*(sigma1**(0.5)))**(0.5))
+        sigma = sigma1+sigma2-2*(torch.sqrt(sigma1 @ sigma2))
         trace = torch.trace(sigma)
         return (mu_dist+trace)[0,0]
 
@@ -37,4 +38,9 @@ if __name__ == "__main__":
     x1 = "./validation/imgs/real/*.jpg"
     x2 = "./validation/imgs/gen/*.jpg"
 
-    print(FID_score(x1,x2))
+    # print(FID_score(x1,x2))
+    from utils.utils import get_hydra_config
+    cfg = get_hydra_config()
+    print(cfg)
+    from data.dataload import dataload 
+    train_dataset, test_dataset = dataload(cfg) 
