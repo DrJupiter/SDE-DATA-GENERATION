17362b016e99a4eda94a1c7526e0ea587beafa96
DrJupiter <klausjupiter@hotmail.com>
2023-02-14 10:57:50 +0100

    fixed name of batch_size

--- data/dataload.py
+++ data/dataload.py
@@ -73,7 +73,7 @@ def dataload(cfg):
         training_generator = NumpyLoader(cifar10_dataset_train, batch_size=cfg.train_and_test.train.batch_size, shuffle=cfg.train_and_test.train.shuffle, num_workers=mp.cpu_count())
 
         cifar10_dataset_test = CIFAR10(cfg.dataset.path, train=False, download=True, transform=FlattenAndCast())
-        training_generator = NumpyLoader(cifar10_dataset_test, batch_size=cfg.train_and_test.test.batch_size, shuffle=cfg.train_and_test.test.shuffle, num_workers=mp.cpu_count())
+        test_generator = NumpyLoader(cifar10_dataset_test, batch_size=cfg.train_and_test.test.batch_size, shuffle=cfg.train_and_test.test.shuffle, num_workers=mp.cpu_count())
 
         return training_generator, test_generator
     
