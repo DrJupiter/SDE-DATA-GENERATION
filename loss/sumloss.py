4d686fbc99d56473e562659e26a842dab475ea72
DrJupiter <klausjupiter@hotmail.com>
2023-03-24 13:27:58 +0100

    implicit score matching loss

---
+++ loss/sumloss.py
@@ -0,0 +1,5 @@
+import jax.numpy as jnp
+def sum_diff_loss(parameters, model_pass, data_batch, target_batch, time_steps, key = None):
+    predictions = model_pass(data_batch, time_steps, parameters) # DDPM = f(x_in, timesteps, parameters)
+    loss = jnp.sum(predictions)-jnp.sum(target_batch)
+    return loss
