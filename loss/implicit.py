a173d9e26752742bb9f983d7d8703c05ad388783
DrJupiter <klausjupiter@hotmail.com>
2023-03-27 10:54:24 +0200

    testing loss oom

--- loss/implicit.py
+++ loss/implicit.py
@@ -1,4 +1,6 @@
-from jax import jacfwd, vmap
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']='false'
+from jax import jacfwd, vmap, jacrev
 import jax.numpy as jnp
 from jax import random as jrandom
 from utils.utils import batch_matmul
@@ -14,7 +16,7 @@ def implicit_score_matching(func, function_parameters, data, time, key):
     The loss that is computed is a numerical estimate of 
         E_(p_D(x))[1/2 ||f(x,θ)||^2 + Div_x(f(x,θ))]
     """
-    hess = jacfwd(func, 0)
+    hess = jacrev(func, 0)
     div = lambda x, t, key: jnp.sum(jnp.diag(hess(x, t, function_parameters, key)))
     #divergence = vmap(div, (0, 0), 0)(data, time.reshape(-1,1)) # TODO: is vmap good here?, ask Paul?
 
@@ -25,29 +27,29 @@ def implicit_score_matching(func, function_parameters, data, time, key):
     print(f"The divergence {divergence}")
     #print(divergence)
 
-    score = func(data, time, function_parameters)
+    score = func(data, time, function_parameters, key)
 
     return jnp.mean(0.5 * batch_matmul(score, score) + divergence)
 
 if __name__ == "__main__":
-    def func(x, timesteps, a):
+    def func(x, timesteps, a, key):
         print(f"The timesteps: {timesteps}")
         
-        return a * x**2
+        return a*x**2 
     from utils.utils import get_hydra_config
     config = get_hydra_config() 
     from data.dataload import dataload
     train, _ = dataload(config)
     iter_train = iter(train)
+    data, label = next(iter_train) 
     key = jrandom.PRNGKey(0)
     key, subkey = jrandom.split(key)
-    data, label = next(iter_train) 
     data = jnp.array([jnp.linspace((1), (10), 10) + _ for _ in range(config.train_and_test.train.batch_size)])
     timesteps = jrandom.uniform(subkey, (data.shape[0],), minval=0, maxval=1)
     print(data)
     a = 1
     print(data)
-    print(f"Function value of data: {func(data, timesteps, a)}")
+    print(f"Function value of data: {func(data, timesteps, a, key)}")
     print(f"Implicit Score matching loss: {implicit_score_matching(func, a, data, timesteps, subkey)}")
     
     
