2476260c87ad5be75bd4c669cca66956146eb0bb
Skroko <73649109+Skroko@users.noreply.github.com>
2023-03-27 09:52:09 +0200

    downscaled and comments

--- models/ddpm/ddpm_unet.py
+++ models/ddpm/ddpm_unet.py
@@ -288,10 +288,12 @@ class up_resnet_attn():
         self.attn2 = attention(cfg, param_asso,sub_model_num, local_num_shift = 8+local_num_shift)
 
     def forward(self, x, embedding, x_res0, x_res1, x_res2, parameters, subkey):
+        """Performs the up-resnet-attn as seen in the paper. Excluding performing the upscaling. This is done in ddpm."""
 
         # split randomness key
         subkey = random.split(subkey*self.sub_model_num,3)
 
+        # perform forward pass of up_resnet with attention excluding the upsampling.
         x = self.resnet0.forward(jnp.concatenate((x,x_res0),axis=-1), embedding, parameters,subkey=subkey[0])
         x = self.attn0.forward(x,parameters)
         x = self.resnet1.forward(jnp.concatenate((x,x_res1),axis=-1), embedding, parameters,subkey=subkey[1])
@@ -306,36 +308,37 @@ class ddpm_unet():
     def __init__(self,cfg) -> None:
 
         # Figure out reshape shape: B x W x H x C
-        if cfg.dataset.name == "mnist":
-            self.shape = (-1, 28, 28, 1)
-        elif cfg.dataset.name == "cifar10":
-            self.shape = (-1, 32, 32, 3)
-        else:
-            raise NameError(f"No suitable shape found for dataset: {cfg.dataset.name}")
+        self.shape = (-1,cfg.dataset.shape[0],cfg.dataset.shape[1],cfg.dataset.shape[2])
 
+        # store for later use
         self.cfg = cfg.model
         conv_shapes = cfg.model.parameters.conv_channels
+        self.upsampling_factor = self.cfg.parameters.upsampling_factor
+        self.embedding_dims = self.cfg.parameters.time_embedding_dims
 
+
+        # get parameter assignments, to pass into submodels
         param_asso = jnp.array(self.cfg.parameters.model_parameter_association)
 
-        # down
+        # initialize submodels
+        ## down
         self.resnet_1 = down_resnet(cfg.model, param_asso,sub_model_num=1,maxpool_factor=2)
         self.resnet_attn_2 = down_resnet_attn(cfg.model, param_asso,sub_model_num=2,maxpool_factor=2)
         self.resnet_3 = down_resnet(cfg.model, param_asso,sub_model_num=3,maxpool_factor=2)
         self.resnet_4 = down_resnet(cfg.model, param_asso,sub_model_num=4,maxpool_factor=1) # no downsampling here
 
-        # middle
+        ## middle
         self.resnet_5 = resnet_ff(cfg.model, param_asso,sub_model_num=5)
         self.attn_6 = attention(cfg.model, param_asso,sub_model_num=6)
         self.resnet_7 = resnet_ff(cfg.model, param_asso,sub_model_num=7)
 
-        # up
+        ## up
         self.resnet_8 = up_resnet(cfg.model, param_asso,sub_model_num=8)
         self.resnet_9 = up_resnet(cfg.model, param_asso,sub_model_num=9)
         self.resnet_attn_10 = up_resnet_attn(cfg.model, param_asso,sub_model_num=10)
         self.resnet_11 = up_resnet(cfg.model, param_asso,sub_model_num=11)
 
-        # end
+        ## end
         self.batchnorm_12 = eqx.experimental.BatchNorm(  # Make 1 for each needed, as they have differetent input shapes
                 input_size=conv_shapes[-1][0], # Its input is equal to last conv input, as this doesnt change shape
                 axis_name="batch",
@@ -343,51 +346,53 @@ class ddpm_unet():
                 eps=cfg.model.parameters.eps,
                 # channelwise_affine=True
                 )
-        self.upsampling_factor = self.cfg.parameters.upsampling_factor
 
     def forward(self, x_in, timesteps, parameters, key = None):
 
-        # Get initial shape
+        # Save initial shape, so we can transform the output pack into this shape
         in_shape = x_in.shape
 
+        # Transform input into the image shape
         x_in = x_in.reshape(self.shape) # TODO: make this work, as cfg is cfg.model, men så kan jeg ikke se train, for shapes
 
-        # split key
+        # Split key to preserve randomness
         if key is not None:
             key, *subkey = random.split(key,13)
         else:
-            key, *subkey = random.split(random.PRNGKey(self.cfg.key),13) 
+            key, *subkey = random.split(random.PRNGKey(self.cfg.model.key),13) # this should only be the case in test cases
 
-        # Timestep embedding
+        # Get parameters for Timestep embedding
         em_w1 = parameters[2][0][-2]
         em_w2 = parameters[2][0][-1]
         em_b1 = parameters[2][1][-2]
         em_b2 = parameters[2][1][-1]
-        embedding_dims = self.cfg.parameters.time_embedding_dims
-        embedding = self.get_timestep_embedding(timesteps, embedding_dim = embedding_dims) # embedding -> dense -> nonlin -> dense (Shape = Bx512)
+
+        # Create the embedding given the timesteps
+        embedding = self.get_timestep_embedding(timesteps, embedding_dim = self.embedding_dims) # embedding -> dense -> nonlin -> dense (Shape = Bx512)
         embedding = jnp.matmul(embedding,em_w1)+em_b1 # 128 -> 512
         embedding = nn.relu(embedding)
         embedding = jnp.matmul(embedding,em_w2)+em_b2 # 512 -> 512
 
-        # down
+        # Perform main pass:
+        ## down
         d0 = lax.conv_general_dilated( 
                 lhs = x_in,    
                 rhs = parameters[0][0], # kernel is the conv [0] and the first entry i this [0]
                 window_strides = [1,1], 
                 padding = 'same',
-                dimension_numbers = ('NHWC', 'HWIO', 'NHWC')
+                dimension_numbers = ('NHWC', 'HWIO', 'NHWC') # input and parameter shapes
                 )
         d10,d11,d12 = self.resnet_1.forward(       d0, embedding, parameters, subkey = subkey[1]) # 32x32 -> 16x16     C_out = 128
         d20,d21,d22 = self.resnet_attn_2.forward(  d12, embedding, parameters, subkey = subkey[2]) # 16x16 -> 8x8      C_out = 256
         d30,d31,d32 = self.resnet_3.forward(       d22, embedding, parameters, subkey = subkey[3]) # 8x8 -> 4x4        C_out = 512
         d40,d41,_   = self.resnet_4.forward(       d32, embedding, parameters, subkey = subkey[4]) # 4x4 -> 4x4        C_out = 512
 
-        # middle
+        ## middle
         m = self.resnet_5.forward(                 d41, embedding, parameters,subkey = subkey[5]) # 4x4 -> 4x4
         m = self.attn_6.forward(                   m, parameters) # 4x4 -> 4x4
         m = self.resnet_7.forward(                 m, embedding, parameters,subkey = subkey[7]) # 4x4 -> 4x4   C_out = 512
 
-        # up
+        ## up
         u = self.resnet_8.forward(          m, embedding, x_res0=d41, x_res1=d40, x_res2=d32, parameters=parameters, subkey = subkey[8]) # 4x4 -> 4x4   C_out = 512
         u = upsample2d(                     u, factor=self.upsampling_factor) # 4x4 -> 8x8
         u = self.resnet_9.forward(          u, embedding, x_res0=d31, x_res1=d30, x_res2=d22, parameters=parameters, subkey = subkey[9]) # 8x8 -> 8x8   C_out = 512
@@ -396,7 +401,7 @@ class ddpm_unet():
         u = upsample2d(                     u, factor=self.upsampling_factor) # 16x16 -> 32x32
         u = self.resnet_11.forward(         u, embedding, x_res0=d11, x_res1=d10, x_res2=d0, parameters=parameters, subkey = subkey[11]) # 32x32 -> 32x32 C_out = 128
 
-        # end
+        #3 end
         e = vmap(self.batchnorm_12,axis_name="batch")(u.transpose(0,3,2,1)).transpose(0,3,2,1)
         e = nn.relu(e)
         e = lax.conv_general_dilated( 
@@ -407,7 +412,7 @@ class ddpm_unet():
                 dimension_numbers = ('NHWC', 'HWIO', 'NHWC')
                 )
         
-        # return to shape loss can take
+        # return to shape loss can take (input shape)
         x_out = e.reshape(in_shape) 
 
         return x_out
@@ -469,15 +474,18 @@ class ddpm_unet():
         return parameters, key
         
     def loss_mse(self, output, true_data):
+        """used for test in this file"""
         return jnp.sum(output)
 
     def loss_fn(self, parameters, true_data, timestep, key = None):
+        """used for test in this file"""
         output = self.forward(true_data, timestep, parameters, key = key)
         loss = self.loss_mse(output, true_data)
         return loss
 
     def get_timestep_embedding(self, timesteps, embedding_dim: int):
         """
+        For math behind this see paper.\n
         timesteps: array of ints describing the timestep each "picture" of the batch is perturbed to.\n
         timesteps.shape = B\n
         From Fairseq.
@@ -488,8 +496,7 @@ class ddpm_unet():
         Credit to DDPM (https://github.com/hojonathanho/diffusion/blob/1e0dceb3b3495bbe19116a5e1b3596cd0706c543/diffusion_tf/nn.py#L90)
         \n I just converted it to jax.
         """
-        print(timesteps)
-        assert len(timesteps.shape) == 1  # and timesteps.dtype == tf.int32
+        assert len(timesteps.shape) == 1 # and timesteps.dtype == tf.int32
 
         half_dim = embedding_dim // 2
         emb = jnp.log(10000) / (half_dim - 1)
