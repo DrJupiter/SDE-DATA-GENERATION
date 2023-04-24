36eb6e482caf03c4ee7c932ebfd0f1512f2775e5
DrJupiter <klausjupiter@hotmail.com>
2023-04-24 10:27:42 +0200

    added none loss for testing purposes

--- loss/loss.py
+++ loss/loss.py
@@ -1,6 +1,7 @@
 
 from loss.implicit import implicit_score_matching
 from loss.dsm import get_denosing_score_matching
+from jax.numpy import zeros
 
 def get_loss(cfg):
     if cfg.loss.name == "denoising_score_matching":
@@ -8,5 +9,5 @@ def get_loss(cfg):
     elif cfg.loss.name == "implicit_score_matching":
         return implicit_score_matching
     elif cfg.loss.name == "none":
-        return lambda *args: 0
+        return lambda *args: zeros(1) 
     raise NotImplementedError(f"The loss {cfg.loss.name} is not implemented")
