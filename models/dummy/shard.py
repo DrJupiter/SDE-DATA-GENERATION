8c6c53ffa50be3d394bb3534aef99ad57fbc04e5
DrJupiter <klausjupiter@hotmail.com>
2023-04-28 14:15:20 +0200

    ready to test

---
+++ models/dummy/shard.py
@@ -0,0 +1,13 @@
+from jax.experimental import mesh_utils
+from jax.sharding import PositionalSharding
+from jax import device_count, device_put 
+import jax
+
+def shard_parameters(parameters):
+    print("Sharding model")
+    n = device_count()
+    sharding = PositionalSharding(mesh_utils.create_device_mesh((n,)))
+    for i in range(len(parameters)):
+        parameters[i] = device_put(parameters[i], sharding.reshape(n, 1))
+        jax.debug.visualize_array_sharding(parameters[i])
+    return parameters
