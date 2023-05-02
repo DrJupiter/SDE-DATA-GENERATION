ef97938d4f80a1398a4ac0c3f71eb51b08ac9cf7
DrJupiter <klausjupiter@hotmail.com>
2023-05-02 15:24:21 +0200

    added decay to optimizer

--- optimizer/optimizers.py
+++ optimizer/optimizers.py
@@ -10,8 +10,16 @@ def get_optim(cfg,params=None):
     raise ValueError(f"Model {cfg.model.name} not found")
 
 def adam(cfg,params):
+    #exponential_decay = optax.warmup_exponential_decay_schedule(1/5000, 1, 0, 5000, 1e-8, 0, False, 2e-4)
+    decay=optax.warmup_cosine_decay_schedule(
+  init_value=0.0,
+  peak_value=1.0,
+  warmup_steps=50,
+  decay_steps=1_000,
+  end_value=0.0,
+    )
     clip = optax.clip_by_global_norm(1.0)
-    adam = optax.adam(learning_rate=cfg.optimizer.learning_rate)
+    adam = optax.adamw(learning_rate=decay)
     optimizer = optax.chain(clip, adam) 
     assert params is not None, "pls give func get_optim the model parameters, for this optimiazation function"
     opt_param = optimizer.init(params)
