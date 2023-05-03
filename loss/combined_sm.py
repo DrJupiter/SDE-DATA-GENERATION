8d52f2c05ec83558b054d6be437087c7876d1363
DrJupiter <klausjupiter@hotmail.com>
2023-05-03 09:23:45 +0200

    weight dsm less

--- loss/combined_sm.py
+++ loss/combined_sm.py
@@ -7,5 +7,5 @@ def get_combined(cfg):
     ism = get_implicit_score_matching(cfg)
     dsm = get_denosing_score_matching(cfg)
     yang_song = get_yang_song(cfg)
-    return lambda func, function_parameters, data, pertubred_data, time, _z, key: yang_song(func, function_parameters, data, pertubred_data, time, _z, key) + dsm(func, function_parameters, data, pertubred_data, time, _z, key)
+    return lambda func, function_parameters, data, pertubred_data, time, _z, key: yang_song(func, function_parameters, data, pertubred_data, time, _z, key) + 0.001 * dsm(func, function_parameters, data, pertubred_data, time, _z, key)
     #return lambda func, function_parameters, data, pertubred_data, time, _z, key: ism(func, function_parameters, data, pertubred_data, time, _z, key) + dsm(func, function_parameters, data, pertubred_data, time, _z, key)
