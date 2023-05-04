1e4688122c28a7281ba2e4d34dc381922b7a9a08
DrJupiter <klausjupiter@hotmail.com>
2023-05-04 08:55:11 +0200

    adamw -> adam

--- optimizer/optimizers.py
+++ optimizer/optimizers.py
@@ -12,7 +12,7 @@ def get_optim(cfg,params=None):
 def adam(cfg,params):
     #exponential_decay = optax.warmup_exponential_decay_schedule(1/5000, 1, 0, 5000, 1e-8, 0, False, 2e-4)
     clip = optax.clip_by_global_norm(1.0)
-    adam = optax.adamw(learning_rate=cfg.optimizer.learning_rate)
+    adam = optax.adam(learning_rate=cfg.optimizer.learning_rate)
     optimizer = optax.chain(clip, adam) 
     assert params is not None, "pls give func get_optim the model parameters, for this optimiazation function"
     opt_param = optimizer.init(params)
