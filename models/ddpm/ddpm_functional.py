b5aefb7540e4cc42f4b418c4812997965a8a73aa
Skroko <73649109+Skroko@users.noreply.github.com>
2023-05-08 15:45:12 +0200

    subkeys are correct in model

--- models/ddpm/ddpm_functional.py
+++ models/ddpm/ddpm_functional.py
@@ -88,7 +88,7 @@ def get_ddpm_unet(cfg, key, inference=False):
         x_in = x_in.reshape(data_shape)
 
         # Split key to preserve randomness
-        key, subkey = random.split(key,2) # TODO: change 2 to 29 and add a * in front of subkey
+        key, *subkey = random.split(key,12) # TODO: change 2 to 29 and add a * in front of subkey
 
         # Create the embedding given the timesteps
         embed = apply_timestep_embedding(timesteps*999, params["p_embed"])
@@ -98,21 +98,21 @@ def get_ddpm_unet(cfg, key, inference=False):
         # Apply model
         x_32_0 = conv1(x_in, params["p_c1"])
 
-        x_32_1, x_32_2, x_16_0 = down1(x_32_0, embed, params["p_d1"], subkey) 
-        x_16_1, x_16_2, x_8_0 = down2_attn(x_16_0, embed, params["p_da2"], subkey) 
-        x_8_1, x_8_2, x_4_0 = down3(x_8_0, embed, params["p_d3"], subkey) 
-        x_4_1, x_4_2, x = down4(x_4_0, embed, params["p_d4"], subkey) 
+        x_32_1, x_32_2, x_16_0 = down1(x_32_0, embed, params["p_d1"], subkey[0]) 
+        x_16_1, x_16_2, x_8_0 = down2_attn(x_16_0, embed, params["p_da2"], subkey[1]) 
+        x_8_1, x_8_2, x_4_0 = down3(x_8_0, embed, params["p_d3"], subkey[2]) 
+        x_4_1, x_4_2, x = down4(x_4_0, embed, params["p_d4"], subkey[3]) 
 
-        x = r1(x, embed, params["p_mr1"], subkey)
-        x = a1(x, embed, params["p_ma2"], subkey)
-        x = r2(x, embed, params["p_mr3"], subkey)
+        x = r1(x, embed, params["p_mr1"], subkey[4])
+        x = a1(x, embed, params["p_ma2"], subkey[5])
+        x = r2(x, embed, params["p_mr3"], subkey[6])
 
         # x = jax.lax.with_sharding_constraint(x, sharding)
 
-        x = up1(x, x_4_2, x_4_1, x_4_0, embed, params["p_u1"], subkey) 
-        x = up2(x, x_8_2, x_8_1, x_8_0, embed, params["p_u2"], subkey)
-        x = up_attn3(x, x_16_2, x_16_1, x_16_0, embed, params["p_ua3"], subkey) 
-        x = up4(x, x_32_2, x_32_1, x_32_0, embed, params["p_u4"], subkey) 
+        x = up1(x, x_4_2, x_4_1, x_4_0, embed, params["p_u1"], subkey[7]) 
+        x = up2(x, x_8_2, x_8_1, x_8_0, embed, params["p_u2"], subkey[8])
+        x = up_attn3(x, x_16_2, x_16_1, x_16_0, embed, params["p_ua3"], subkey[9]) 
+        x = up4(x, x_32_2, x_32_1, x_32_0, embed, params["p_u4"], subkey[10]) 
 
         x = conv2(x, params["p_c2"])
 
@@ -136,7 +136,7 @@ def get_ddpm_unet(cfg, key, inference=False):
         x_in = x_in.reshape(data_shape)
 
         # Split key to preserve randomness
-        key, subkey = random.split(key,2) # TODO: change 2 to 29 and add a * in front of subkey
+        key, subkey = random.split(key,12) # TODO: change 2 to 29 and add a * in front of subkey
 
         # Create the embedding given the timesteps
         embed = apply_timestep_embedding(timesteps*999, params["p_embed"])
@@ -146,21 +146,21 @@ def get_ddpm_unet(cfg, key, inference=False):
         # Apply model
         x_32_0 = inf_conv1(x_in, params["p_c1"])
 
-        x_32_1, x_32_2, x_16_0 = inf_down1(x_32_0, embed, params["p_d1"], subkey) 
-        x_16_1, x_16_2, x_8_0 = inf_down2_attn(x_16_0, embed, params["p_da2"], subkey)
-        x_8_1, x_8_2, x_4_0 = inf_down3(x_8_0, embed, params["p_d3"], subkey) 
-        x_4_1, x_4_2, x = inf_down4(x_4_0, embed, params["p_d4"], subkey) 
+        x_32_1, x_32_2, x_16_0 = inf_down1(x_32_0, embed, params["p_d1"], subkey[0]) 
+        x_16_1, x_16_2, x_8_0 = inf_down2_attn(x_16_0, embed, params["p_da2"], subkey[1])
+        x_8_1, x_8_2, x_4_0 = inf_down3(x_8_0, embed, params["p_d3"], subkey[2]) 
+        x_4_1, x_4_2, x = inf_down4(x_4_0, embed, params["p_d4"], subkey[3]) 
 
-        x = inf_r1(x, embed, params["p_mr1"], subkey)
-        x = inf_a1(x, embed, params["p_ma2"], subkey)
-        x = inf_r2(x, embed, params["p_mr3"], subkey)
+        x = inf_r1(x, embed, params["p_mr1"], subkey[4])
+        x = inf_a1(x, embed, params["p_ma2"], subkey[5])
+        x = inf_r2(x, embed, params["p_mr3"], subkey[6])
 
         # x = jax.lax.with_sharding_constraint(x, sharding)
 
-        x = inf_up1(x, x_4_2, x_4_1, x_4_0, embed, params["p_u1"], subkey)
-        x = inf_up2(x, x_8_2, x_8_1, x_8_0, embed, params["p_u2"], subkey) 
-        x = inf_up_attn3(x, x_16_2, x_16_1, x_16_0, embed, params["p_ua3"], subkey) 
-        x = inf_up4(x, x_32_2, x_32_1, x_32_0, embed, params["p_u4"], subkey)
+        x = inf_up1(x, x_4_2, x_4_1, x_4_0, embed, params["p_u1"], subkey[7])
+        x = inf_up2(x, x_8_2, x_8_1, x_8_0, embed, params["p_u2"], subkey[8]) 
+        x = inf_up_attn3(x, x_16_2, x_16_1, x_16_0, embed, params["p_ua3"], subkey[9]) 
+        x = inf_up4(x, x_32_2, x_32_1, x_32_0, embed, params["p_u4"], subkey[10])
 
         x = inf_conv2(x, params["p_c2"])
 
