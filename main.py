562fb1aefa422b48a23ba20998f5ef10aa36718b
DrJupiter <klausjupiter@hotmail.com>
2023-05-17 14:30:31 +0200

    validation fid correcting for shape

--- main.py
+++ main.py
@@ -238,14 +238,14 @@ def run_experiment(cfg):
           arg = [x[i*split_factor:(i+1)*split_factor] for x in args]
           generated_imgs = jax.vmap(get_sample, (0, 0, 0, 0, 0))(*arg)
           all_generated_imgs += list(generated_imgs)
-
+          break
         all_generated_imgs = jnp.array(all_generated_imgs)
 
         display_images(cfg, all_generated_imgs[:15], all_labels.reshape(-1)[:15], log_title="Perturbed 0 -> x(0)")
-        fid = fid_model(all_generated_imgs, all_data)
+        fid = fid_model(all_generated_imgs[:2], all_data[:2])
         wandb.log({"FID GEN x DATA": fid})
         # sanity check
-        fid_data = fid_model(all_data, all_data)
+        fid_data = fid_model(all_data[:2], all_data[:2])
         wandb.log({"FID DATA x DATA": fid})
         
         
