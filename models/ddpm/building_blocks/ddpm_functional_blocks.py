ef2eb9dfe88d21db44dc8ddd760eb5b66cd23869
DrJupiter <klausjupiter@hotmail.com>
2023-05-30 13:32:37 +0200

    guidance fix

--- models/ddpm/building_blocks/ddpm_functional_blocks.py
+++ models/ddpm/building_blocks/ddpm_functional_blocks.py
@@ -313,8 +313,8 @@ def get_conv(cfg, key, in_C, out_C,first=False):
     #params = abf*random.normal(key, ((kernel_size, kernel_size, in_C, out_C)), dtype=jnp.float32)
     n_devices = len(jax.devices())
 
-    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
-    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
+    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
+    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
 
     # if first:
     #     sharding = sharding.reshape(1,1,1,-1)
@@ -367,8 +367,8 @@ def get_resnet_ff(cfg, key, in_C, out_C):
     dropout, _ = get_dropout(cfg, key, in_C, out_C)
 
     n_devices = len(jax.devices())
-    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
-    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
+    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
+    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
 
 
     @jit
@@ -473,8 +473,8 @@ def get_attention(cfg, key, in_C, out_C):
     # batchnorm, params["btchN1"] = get_batchnorm(cfg, key, in_C, out_C)
 
     n_devices = len(jax.devices())
-    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
-    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
+    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
+    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
 
 
     @jit
@@ -521,8 +521,8 @@ def get_down(cfg, key, in_C, out_C, factor):
     params = {"r1":params1, "r2": params2}
 
     n_devices = len(jax.devices())
-    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
-    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
+    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
+    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
 
 
     factor = factor
@@ -588,8 +588,8 @@ def get_up(cfg, key, in_C, out_C, residual_C: list, factor):
     params = {"r1":params1, "r2": params2,"r3": params3}
 
     n_devices = len(jax.devices())
-    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
-    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
+    sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(n_devices,1,1,1)
+    #sharding = PositionalSharding(mesh_utils.create_device_mesh((n_devices,))).reshape(1,n_devices,1,1)
 
     factor = factor
 
