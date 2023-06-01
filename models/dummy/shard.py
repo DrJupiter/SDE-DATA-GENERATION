0559f3757755fa39b798c8e45d5c201b6e8a6b01
DrJupiter <klausjupiter@hotmail.com>
2023-06-01 10:33:21 +0200

    tmp

--- models/dummy/shard.py
+++ models/dummy/shard.py
@@ -1,11 +1,14 @@
 from jax.experimental import mesh_utils
-from jax.sharding import PositionalSharding
+from jax.sharding import PositionalSharding, NamedSharding, PartitionSpec
 from jax import device_count, device_put 
 import jax
+import utils.sharding
 
 def shard_parameters(cfg,parameters):
     print("Sharding model")
     n = device_count()
+    names, mesh = utils.sharding.get_sharding(cfg)
+
     sharding = PositionalSharding(mesh_utils.create_device_mesh((n,)))
     for i in range(len(parameters)):
         parameters[i] = device_put(parameters[i], sharding.reshape(n, 1))
