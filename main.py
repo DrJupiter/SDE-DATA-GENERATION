5d1b8747c191e7b527f52d15228980eefcf7c8f5
DrJupiter <klausjupiter@hotmail.com>
2023-06-06 08:23:40 +0200

    updated lower bound time for sampling to match paper

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ def run_experiment(cfg):
                       @ft.partial(shard_map, mesh=mesh, in_specs=generation_spec, out_specs=generation_spec, check_rep=False)
                       @jax.vmap
                       def get_sample(t, key1, key0, xt, text_embedding):
-                        return sample(jnp.array(1e-5,dtype=jnp.float32), jnp.array(0,dtype=jnp.float32), t.astype(float)[0], jnp.array([-1/1000]).astype(float)[0], drift_test, diffusion_test, [inference_model, text_embedding,model_parameters if cfg.model.name != "sde" else data[0], key0], xt, key1) 
+                        return sample(jnp.array(1e-3,dtype=jnp.float32), jnp.array(0,dtype=jnp.float32), t.astype(float)[0], jnp.array([-1/1000]).astype(float)[0], drift_test, diffusion_test, [inference_model, text_embedding,model_parameters if cfg.model.name != "sde" else data[0], key0], xt, key1) 
 
                       #get_sample = lambda t, key1, key0, xt, text_embedding: sample(1e-5, 0, t.astype(float)[0], -1/1000, drift, diffusion, [inference_model, text_embedding,model_parameters if cfg.model.name != "sde" else data[0], key0], xt, key1) 
                                       # dt0 = - 1/N
@@ -303,7 +303,7 @@ def run_experiment(cfg):
           @ft.partial(shard_map, mesh=mesh, in_specs=generation_spec, out_specs=generation_spec, check_rep=False)
           @jax.vmap
           def get_sample(t, key1, key0, xt, text_embedding):
-            return sample(jnp.array(1e-5,dtype=jnp.float32), jnp.array(0,dtype=jnp.float32), t.astype(float)[0], jnp.array([-1/1000]).astype(float)[0], drift_test, diffusion_test, [inference_model, text_embedding,model_parameters if cfg.model.name != "sde" else data[0], key0], xt, key1)
+            return sample(jnp.array(1e-3,dtype=jnp.float32), jnp.array(0,dtype=jnp.float32), t.astype(float)[0], jnp.array([-1/1000]).astype(float)[0], drift_test, diffusion_test, [inference_model, text_embedding,model_parameters if cfg.model.name != "sde" else data[0], key0], xt, key1)
 
           #get_sample = lambda t, key1, key0, xt, text_embedding: sample(1e-5, 0, t.astype(float)[0], -1/1000, drift, diffusion, [inference_model, text_embedding,model_parameters if cfg.model.name != "sde" else all_data[0], key0], xt, key1) 
 
