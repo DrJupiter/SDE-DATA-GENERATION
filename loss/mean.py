bdc779a6c8ccef6b6a96388375a389925febd5bb
DrJupiter <klausjupiter@hotmail.com>
2023-08-19 12:10:28 +0200

    mean loss test

--- loss/mean.py
+++ loss/mean.py
@@ -1,6 +1,7 @@
 
 from utils.utility import batch_matmul
 from jax import numpy as jnp
+from jax import vmap
 from sde.sde import get_sde
 
 def get_mean_matching(cfg):
@@ -9,11 +10,11 @@ def get_mean_matching(cfg):
  
     def mean_matching(func, function_parameters, data, perturbed_data, time, _z, text_embedding, key):
 
-        mean, _cov = sde.parameters(time, data)
+        mean, cov = sde.parameters(time, data)
 
         difference = func(perturbed_data, time, text_embedding, function_parameters, key)-mean
         #return 0.5 * jnp.mean(batch_matmul(difference, difference))
-        difference_length = jnp.square(jnp.linalg.norm(difference))
+        difference_length = jnp.square(jnp.linalg.norm(vmap(lambda a,b: a * b)(-1/cov,(difference))))
         return 0.5 * jnp.mean(difference_length)
     
 
--- sde/sde.py
+++ sde/sde.py
@@ -1,10 +1,13 @@
 #from subvp import SUBVPSDE 
 from sde.subvp import SUBVPSDE 
+from sde.denoise import DENOISESDE
 
 
 def get_sde(cfg):
     if cfg.sde.name == "paper_subvp":
         return SUBVPSDE(cfg)
+    if cfg.sde.name == "denoise":
+        return DENOISESDE(cfg)
     raise NameError(f"No SDE found with the name {cfg.sde.name}")
 
 
